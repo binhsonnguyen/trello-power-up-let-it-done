9816f20f73ab53bb727fd7cf709bcb2bb1650f3b
edward <son@binhsonnguyen.com>
2020-02-23 20:08:15 +0700

    log to console when client ready

--- public/js/client.js
+++ public/js/client.js
@@ -1,3 +1,5 @@
+console.log('client loaded');
+
 const BLACK_ROCKET_ICON = 'https://cdn.glitch.com/1b42d7fe-bda8-4af8-a6c8-eff0cea9e08a%2Frocket-ship.png?1494946700421';
 
 const GRAY_ICON = 'https://cdn.hyperdev.com/us-east-1%3A3d31b21c-01a0-4da2-8827-4bc6e88b7618%2Ficon-gray.svg';
